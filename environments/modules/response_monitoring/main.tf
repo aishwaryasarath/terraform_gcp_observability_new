e8690c22a0597ee64f65e25035e5be5da4003d14
Aiswarya <aishwaryasarath04@gmail.com>
2025-08-24 11:03:31 -0500

    added alerts:wq

--- environments/modules/response_monitoring/main.tf
+++ environments/modules/response_monitoring/main.tf
@@ -17,20 +17,20 @@ resource "google_monitoring_alert_policy" "gcs_4xx_error" {
     display_name = "GCS Bucket - 4xx Request count"
 
     condition_threshold {
-      filter          = "resource.type = \"gcs_bucket\" AND resource.labels.bucket_name = \"${var.bucket_name}\" AND metric.type = \"storage.googleapis.com/api/request_count\" AND metric.labels.response_code = monitoring.regex.full_match(\"CANCELLED|INVALID_ARGUMENT|NOT_FOUND|ALREADY_EXISTS|PERMISSION_DENIED|UNAUTHENTICATED|RESOURCE_EXHAUSTED|FAILED_PRECONDITION|ABORTED|OUT_OF_RANGE\")"
-      comparison      = "COMPARISON_GT"
-      duration        = "300s"
-      threshold_value = 10
+      filter     = "resource.type = \"gcs_bucket\" AND resource.labels.bucket_name = \"${var.bucket_name}\" AND metric.type = \"storage.googleapis.com/api/request_count\" AND metric.labels.response_code = monitoring.regex.full_match(\"CANCELLED|INVALID_ARGUMENT|NOT_FOUND|ALREADY_EXISTS|PERMISSION_DENIED|UNAUTHENTICATED|RESOURCE_EXHAUSTED|FAILED_PRECONDITION|ABORTED|OUT_OF_RANGE\")"
+      comparison = "COMPARISON_GT"
+      duration   = "60s"
+      #duration = "300s"
+      #threshold_value = 10
+      threshold_value = 5
 
       trigger {
         count = 1
       }
 
       aggregations {
-        alignment_period     = "300s"
-        per_series_aligner   = "ALIGN_DELTA"
-        cross_series_reducer = "REDUCE_SUM"
-        group_by_fields      = ["metric.label.response_code"]
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_DELTA"
       }
     }
   }
@@ -60,9 +60,10 @@ resource "google_monitoring_alert_policy" "gcs_5xx_error" {
     display_name = "GCS Bucket - 5xx Request count"
 
     condition_threshold {
-      filter          = "resource.type = \"gcs_bucket\" AND resource.labels.bucket_name = \"${var.bucket_name}\" AND metric.type = \"storage.googleapis.com/api/request_count\" AND metric.labels.response_code = monitoring.regex.full_match(\"INTERNAL|UNIMPLEMENTED|UNAVAILABLE|DATA_LOSS|DEADLINE_EXCEEDED|UNKNOWN\")"
-      comparison      = "COMPARISON_GT"
-      duration        = "300s"
+      filter     = "resource.type = \"gcs_bucket\" AND resource.labels.bucket_name = \"${var.bucket_name}\" AND metric.type = \"storage.googleapis.com/api/request_count\" AND metric.labels.response_code = monitoring.regex.full_match(\"INTERNAL|UNIMPLEMENTED|UNAVAILABLE|DATA_LOSS|DEADLINE_EXCEEDED|UNKNOWN\")"
+      comparison = "COMPARISON_GT"
+      duration   = "60s"
+      #duration = "300s"
       threshold_value = 10
 
       trigger {
@@ -70,10 +71,9 @@ resource "google_monitoring_alert_policy" "gcs_5xx_error" {
       }
 
       aggregations {
-        alignment_period     = "300s"
-        per_series_aligner   = "ALIGN_DELTA"
-        cross_series_reducer = "REDUCE_SUM"
-        group_by_fields      = ["metric.label.response_code"]
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_DELTA"
+        #group_by_fields = ["metric.label.response_code"]
       }
     }
   }
@@ -86,134 +86,193 @@ resource "google_monitoring_alert_policy" "gcs_5xx_error" {
   notification_channels = var.notification_channel_ids
 }
 
+# main.tf
+resource "google_monitoring_alert_policy" "secret_4xx_error" {
+  display_name = "4xx secret manager request count"
+  combiner     = "OR"
+  enabled      = true
+  severity     = "WARNING"
 
-# resource "google_monitoring_alert_policy" "gcs_500_error" {
-#   project      = var.project
-#   display_name = "test gcs 500 - ${var.bucket_name}"
-#   combiner     = "OR"
-#   enabled      = true
-#   severity     = "ERROR"
+  documentation {
+    content   = "test $${metric.label.response_code}."
+    subject   = "test secret 4xx"
+    mime_type = "text/markdown"
+  }
 
-#   documentation {
-#     content   = "test ${var.bucket_name}"
-#     mime_type = "text/markdown"
-#     subject   = "test- alert gcs manual"
-#   }
+  conditions {
+    display_name = "4xx Secret Manager Request count"
 
-#   conditions {
-#     display_name = "GCS Bucket - Request count"
+    condition_threshold {
+      filter = <<-EOT
+        resource.type = "consumed_api"
+        AND resource.labels.service = "secretmanager.googleapis.com"
+        AND metric.type = "serviceruntime.googleapis.com/api/request_count"
+        AND metric.labels.response_code_class = "4xx"
+      EOT
 
-#     condition_threshold {
-#       filter          = "resource.type = \"gcs_bucket\" AND resource.labels.bucket_name = \"${var.bucket_name}\" AND metric.type = \"storage.googleapis.com/api/request_count\" AND metric.labels.response_code = monitoring.regex.full_match(\"INTERNAL|UNIMPLEMENTED|UNAVAILABLE|DATA_LOSS|DEADLINE_EXCEEDED|UNKNOWN\")"
-#       comparison      = "COMPARISON_GT"
-#       duration        = "0s"
-#       threshold_value = 5
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_DELTA"
+      }
 
-#       trigger {
-#         count = 1
-#       }
+      comparison      = "COMPARISON_GT"
+      threshold_value = 5
+      duration        = "60s"
+      #duration        = "300s"
 
-#       aggregations {
-#         alignment_period     = "300s"
-#         per_series_aligner   = "ALIGN_RATE"
-#         cross_series_reducer = "REDUCE_SUM"
-#         group_by_fields      = ["metric.label.response_code"]
-#       }
-#     }
-#   }
+      trigger {
+        count = 1
+      }
+    }
+  }
 
-#   alert_strategy {
-#     auto_close           = "3600s"
-#     notification_prompts = ["OPENED"]
-#   }
+  alert_strategy {
+    auto_close           = "1800s"
+    notification_prompts = ["OPENED", "CLOSED"]
+  }
 
-#   notification_channels = var.notification_channel_ids
-# }
+  notification_channels = var.notification_channel_ids
+}
 
-# resource "google_monitoring_alert_policy" "gcs_4xx_error" {
-#   project      = var.project
-#   display_name = "test gcs 4xx - ${var.bucket_name}"
-#   combiner     = "OR"
-#   enabled      = true
-#   severity     = "ERROR"
+resource "google_monitoring_alert_policy" "secret_5xx_error" {
+  display_name = "5xx secret manager request count"
+  combiner     = "OR"
+  enabled      = true
+  severity     = "WARNING"
 
-#   documentation {
-#     content   = "4xx error alert for ${var.bucket_name} "
-#     mime_type = "text/markdown"
-#     subject   = "test- alert gcs 4xx manual"
-#   }
+  documentation {
+    content   = "$${metric.label.response_code}."
+    subject   = "secret 5xx"
+    mime_type = "text/markdown"
+  }
 
-#   conditions {
-#     display_name = "GCS Bucket - 4xx Request count"
+  conditions {
+    display_name = "5xx Secret Manager Request count"
 
-#     condition_threshold {
-#       filter          = "resource.type = \"gcs_bucket\" AND resource.labels.bucket_name = \"${var.bucket_name}\" AND metric.type = \"storage.googleapis.com/api/request_count\" AND metric.labels.response_code = monitoring.regex.full_match(\"CANCELLED|INVALID_ARGUMENT|NOT_FOUND|ALREADY_EXISTS|PERMISSION_DENIED|UNAUTHENTICATED|RESOURCE_EXHAUSTED|FAILED_PRECONDITION|ABORTED|OUT_OF_RANGE\")"
-#       comparison      = "COMPARISON_GT"
-#       duration        = "0s"
-#       threshold_value = 1
+    condition_threshold {
+      filter = <<-EOT
+        resource.type = "consumed_api"
+        AND resource.labels.service = "secretmanager.googleapis.com"
+        AND metric.type = "serviceruntime.googleapis.com/api/request_count"
+        AND metric.labels.response_code_class = "5xx"
+      EOT
 
-#       trigger {
-#         count = 1
-#       }
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_DELTA"
+      }
 
-#       aggregations {
-#         alignment_period     = "300s"
-#         per_series_aligner   = "ALIGN_RATE"
-#         cross_series_reducer = "REDUCE_SUM"
-#         group_by_fields      = ["metric.label.response_code"]
-#       }
-#     }
-#   }
+      comparison      = "COMPARISON_GT"
+      threshold_value = 5
+      duration        = "60s"
+      #duration        = "300s"
 
-#   alert_strategy {
-#     auto_close           = "3600s"
-#     notification_prompts = ["OPENED"]
-#   }
+      trigger {
+        count = 1
+      }
+    }
+  }
 
-#   notification_channels = var.notification_channel_ids
-# }
+  alert_strategy {
+    auto_close           = "1800s"
+    notification_prompts = ["OPENED", "CLOSED"]
+  }
 
+  notification_channels = var.notification_channel_ids
+}
 
-# resource "google_monitoring_alert_policy" "gcs_4xx_error" {
-#   project      = var.project
-#   display_name = "test gcs 4xx - ${var.bucket_name}"
-#   combiner     = "OR"
-#   enabled      = true
-#   severity     = "ERROR"
+#for vertex ai
+#resource.type="consumed_api" AND metric.type="serviceruntime.googleapis.com/api/request_count"  AND resource.labels.service="aiplatform.googleapis.com" AND metric.labels.response_code_class = "4xx")
+resource "google_monitoring_alert_policy" "vertex_ai_4xx_error" {
+  display_name = "4xx vertex ai request count"
+  combiner     = "OR"
+  enabled      = true
+  severity     = "WARNING"
 
-#   documentation {
-#     content   = "4xx error alert for ${var.bucket_name} "
-#     mime_type = "text/markdown"
-#     subject   = "test- alert gcs 4xx manual"
-#   }
+  documentation {
+    content   = "$${metric.label.response_code}."
+    subject   = "vertex ai 4xx"
+    mime_type = "text/markdown"
+  }
 
-#   conditions {
-#     display_name = "GCS Bucket - 4xx Request count"
+  conditions {
+    display_name = "4xx vertex ai  Request count"
 
-#     condition_threshold {
-#       filter          = "resource.type = \"gcs_bucket\" AND resource.labels.bucket_name = \"${var.bucket_name}\" AND metric.type = \"storage.googleapis.com/api/request_count\" AND metric.labels.response_code = monitoring.regex.full_match(\"CANCELLED|INVALID_ARGUMENT|NOT_FOUND|ALREADY_EXISTS|PERMISSION_DENIED|UNAUTHENTICATED|RESOURCE_EXHAUSTED|FAILED_PRECONDITION|ABORTED|OUT_OF_RANGE\")"
-#       comparison      = "COMPARISON_GT"
-#       duration        = "300s"
-#       threshold_value = 10
+    condition_threshold {
+      filter = <<-EOT
+        resource.type = "consumed_api"
+        AND resource.labels.service = "aiplatform.googleapis.com"
+        AND metric.type = "serviceruntime.googleapis.com/api/request_count"
+        AND metric.labels.response_code_class = "4xx"
+      EOT
 
-#       trigger {
-#         count = 1
-#       }
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_DELTA"
+      }
 
-#       aggregations {
-#         alignment_period   = "60s"
-#         per_series_aligner = "ALIGN_DELTA"
-#         # cross_series_reducer = "REDUCE_SUM"
-#         group_by_fields = ["metric.label.response_code"]
-#       }
-#     }
-#   }
+      comparison      = "COMPARISON_GT"
+      threshold_value = 5
+      duration        = "60s"
+      #duration        = "300s"
 
-#   alert_strategy {
-#     auto_close           = "3600s"
-#     notification_prompts = ["OPENED"]
-#   }
+      trigger {
+        count = 1
+      }
+    }
+  }
 
-#   notification_channels = var.notification_channel_ids
-# }
+  alert_strategy {
+    auto_close           = "1800s"
+    notification_prompts = ["OPENED", "CLOSED"]
+  }
 
+  notification_channels = var.notification_channel_ids
+}
+
+resource "google_monitoring_alert_policy" "vertex_ai_5xx_error" {
+  display_name = "5xx vertex ai request count"
+  combiner     = "OR"
+  enabled      = true
+  severity     = "WARNING"
+
+  documentation {
+    content   = "$${metric.label.response_code}."
+    subject   = "vertex ai 5xx"
+    mime_type = "text/markdown"
+  }
+
+  conditions {
+    display_name = "5xx vertex ai Request count"
+
+    condition_threshold {
+      filter = <<-EOT
+        resource.type = "consumed_api"
+        AND resource.labels.service = "aiplatform.googleapis.com"
+        AND metric.type = "serviceruntime.googleapis.com/api/request_count"
+        AND metric.labels.response_code_class = "5xx"
+      EOT
+
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_DELTA"
+      }
+
+      comparison      = "COMPARISON_GT"
+      threshold_value = 5
+      duration        = "60s"
+      #duration        = "300s"
+
+      trigger {
+        count = 1
+      }
+    }
+  }
+
+  alert_strategy {
+    auto_close           = "1800s"
+    notification_prompts = ["OPENED", "CLOSED"]
+  }
+
+  notification_channels = var.notification_channel_ids
+}
--- environments/modules/secretmanager_monitoring/main.tf
+++ environments/modules/secretmanager_monitoring/main.tf
@@ -2,9 +2,22 @@
 
 resource "google_logging_metric" "secret_critical_errors" {
   for_each = {
-    secret_manager_internal_error      = "resource.type=\"secretmanager.googleapis.com/Secret\" AND protoPayload.status.code=13"
-    secret_manager_service_unavailable = "resource.type=\"secretmanager.googleapis.com/Secret\" AND protoPayload.status.code=14"
-    secret_manager_excessive_access    = "resource.type=\"audited_resource\" AND protoPayload.methodName=\"google.cloud.secretmanager.v1.SecretManagerService.AccessSecretVersion\""
+    secret_manager_cancelled_499           = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=1"
+    secret_manager_unknown_500             = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=2"
+    secret_manager_invalid_argument_400    = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=3"
+    secret_manager_deadline_exceeded_504   = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=4"
+    secret_manager_not_found_404           = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=5"
+    secret_manager_already_exists_409      = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=6"
+    secret_manager_permission_denied_403   = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=7"
+    secret_manager_resource_exhausted_429  = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=8"
+    secret_manager_failed_precondition_400 = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=9"
+    secret_manager_aborted_409             = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=10"
+    secret_manager_out_of_range_400        = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=11"
+    secret_manager_unimplemented_501       = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=12"
+    secret_manager_internal_error_500      = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=13"
+    secret_manager_unavailable_503         = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=14"
+    secret_manager_data_loss_500           = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=15"
+    secret_manager_unauthenticated_401     = "resource.type=\"audited_resource\" AND resource.labels.service=\"secretmanager.googleapis.com\" AND protoPayload.status.code=16"
   }
 
   name        = each.key
@@ -19,63 +32,26 @@ resource "google_logging_metric" "secret_critical_errors" {
 
 }
 
-resource "google_monitoring_alert_policy" "secret_manager_excessive_access" {
-  display_name = "secret_manager_excessive_access"
-  combiner     = "OR"
-  enabled      = true
-  severity     = "WARNING"
-
-  depends_on = [
-    google_logging_metric.secret_critical_errors
-  ]
-
-  documentation {
-    content   = <<-EOT
-    A secret version has been accessed excessively. This may indicate a potential security issue or misconfiguration.
-    For detailed investigation, check this runbook link: insert link here
-  EOT
-    mime_type = "text/markdown"
-    subject   = "secret excessive access"
-  }
-
-  conditions {
-    display_name = "Audited Resource - logging/user/excessive_access"
-
-    condition_threshold {
-      filter          = "resource.type = \"audited_resource\" AND metric.type = \"logging.googleapis.com/user/secret_manager_excessive_access\""
-      duration        = "60s" #300s
-      comparison      = "COMPARISON_GT"
-      threshold_value = 20
-
-      trigger {
-        count = 1
-      }
-
-      aggregations {
-        alignment_period     = "60s" #600s
-        per_series_aligner   = "ALIGN_DELTA"
-        cross_series_reducer = "REDUCE_SUM"
-      }
-    }
-  }
-
-  alert_strategy {
-    auto_close = "604800s" # 7 days
-
-    notification_prompts = ["OPENED"]
-  }
-
-  notification_channels = var.notification_channel_ids
-}
-
-
-
 
 
 resource "google_monitoring_alert_policy" "secret_error_alerts" {
   for_each = {
-    internal_error      = "logging.googleapis.com/user/internal_error"
-    service_unavailable = "logging.googleapis.com/user/service_unavailable"
+    secret_manager_cancelled_499           = "logging.googleapis.com/user/secret_manager_cancelled_499"
+    secret_manager_unknown_500             = "logging.googleapis.com/user/secret_manager_unknown_500"
+    secret_manager_invalid_argument_400    = "logging.googleapis.com/user/secret_manager_invalid_argument_400"
+    secret_manager_deadline_exceeded_504   = "logging.googleapis.com/user/secret_manager_deadline_exceeded_504"
+    secret_manager_not_found_404           = "logging.googleapis.com/user/secret_manager_not_found_404"
+    secret_manager_already_exists_409      = "logging.googleapis.com/user/secret_manager_already_exists_409"
+    secret_manager_permission_denied_403   = "logging.googleapis.com/user/secret_manager_permission_denied_403"
+    secret_manager_resource_exhausted_429  = "logging.googleapis.com/user/secret_manager_resource_exhausted_429"
+    secret_manager_failed_precondition_400 = "logging.googleapis.com/user/secret_manager_failed_precondition_400"
+    secret_manager_aborted_409             = "logging.googleapis.com/user/secret_manager_aborted_409"
+    secret_manager_out_of_range_400        = "logging.googleapis.com/user/secret_manager_out_of_range_400"
+    secret_manager_unimplemented_501       = "logging.googleapis.com/user/secret_manager_unimplemented_501"
+    secret_manager_internal_error_500      = "logging.googleapis.com/user/secret_manager_internal_error_500"
+    secret_manager_unavailable_503         = "logging.googleapis.com/user/secret_manager_unavailable_503"
+    secret_manager_data_loss_500           = "logging.googleapis.com/user/secret_manager_data_loss_500"
+    secret_manager_unauthenticated_401     = "logging.googleapis.com/user/secret_manager_unauthenticated_401"
   }
   depends_on = [
     google_logging_metric.secret_critical_errors
@@ -84,88 +60,15 @@ resource "google_monitoring_alert_policy" "secret_error_alerts" {
   combiner     = "OR"
 
   conditions {
-    display_name = "${each.key} rate"
+    display_name = "${each.key} errors"
     condition_threshold {
-      filter          = "resource.type=\"secretmanager.googleapis.com/Secret\" AND metric.type=\"${each.value}\""
+      filter          = "resource.type=\"audited_resource\" AND metric.type=\"${each.value}\""
       duration        = "60s"
       comparison      = "COMPARISON_GT"
       threshold_value = 1
       aggregations {
         alignment_period   = "60s"
         per_series_aligner = "ALIGN_RATE"
-      }
-    }
-  }
-}
-
-resource "google_monitoring_alert_policy" "secret_deleted_alert" {
-  display_name = "Secret Manager - Secret Deleted"
-
-  combiner = "OR"
-  severity = "CRITICAL"
-
-  conditions {
-    display_name = "Secret Deleted"
-    condition_matched_log {
-      filter = "resource.type=\"audited_resource\" AND protoPayload.methodName=\"google.cloud.secretmanager.v1.SecretManagerService.DeleteSecret\""
-    }
-  }
-  notification_channels = var.notification_channel_ids
-  documentation {
-    content   = "A secret was deleted from Secret Manager. For detailed investigation, check this runbook link: insert link here"
-    mime_type = "text/markdown"
-  }
-  alert_strategy {
-    notification_rate_limit {
-      period = "3600s" # limit notifications to at most one per hour
-    }
-    auto_close = "86400s"
-  }
-
-}
-resource "google_logging_metric" "secret_unauthorized_access_metric" {
-  name   = "secret_unauthorized_access_count"
-  filter = <<EOT
-resource.type="audited_resource"
-protoPayload.authorizationInfo.permission="secretmanager.versions.access"
-protoPayload.authorizationInfo.granted="false"
-EOT
-
-  metric_descriptor {
-    metric_kind  = "DELTA"
-    value_type   = "INT64"
-    unit         = "1"
-    display_name = "Secret Unauthorized Access"
-  }
-}
-
-resource "google_monitoring_alert_policy" "secret_unauthorized_access_count" {
-
-  display_name = "secret_unauthorized_access_count"
-  combiner     = "OR"
-  enabled      = true
-  severity     = "ERROR"
-
-  depends_on = [
-    google_logging_metric.secret_unauthorized_access_metric
-  ]
-
-  conditions {
-    display_name = "Secret Unauthorized Access attempt Detected"
-
-    condition_threshold {
-      filter          = "resource.type=\"audited_resource\" AND metric.type=\"logging.googleapis.com/user/secret_unauthorized_access_count\""
-      comparison      = "COMPARISON_GT"
-      threshold_value = 0
-      duration        = "0s"
-
-      aggregations {
-        alignment_period   = "60s"
-        per_series_aligner = "ALIGN_DELTA"
-      }
-
-      trigger {
-        count = 1
       }
     }
   }
